1b6fd4ad6a7ac6a84c317c1f50aa642e3a3a3a1e
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-31 15:50:52 -0600

    added login/register/newpost/firebaseAuth

---
+++ app/shared/components/homeController.js
@@ -0,0 +1,77 @@
+app.controller('homeCtrl', function($scope, $location, authFactory) {
+  $scope.logout = () => {
+    authFactory.logout()
+    .then(() => console.log('logged out'))
+  }
+
+    $scope.userLogin = () => {
+
+        authFactory.login($scope.user_email, $scope.user_password)
+            .then(() => console.log("woohoo"))
+    }
+    $scope.createUser = () => {
+        console.log($scope.user_email)
+        authFactory.createUser($scope.user_email, $scope.user_password)
+            .then(() => console.log("success"))
+
+    }
+
+
+    //materialize Modals below
+
+    $('#loginButton').click(() => {
+        $('#loginModal').modal('open')
+    })
+
+    $('.registerButton').click(() => {
+        $('#registerModal').modal('open')
+    })
+
+    $('#newPost').click(() => {
+            $('#postModal').modal('open')
+        })
+        //login
+    $('#loginModal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .3, // Opacity of modal background
+        inDuration: 700, // Transition in duration
+        outDuration: 700, // Transition out duration
+        startingTop: '0%', // Starting top style attribute
+        endingTop: '20%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+
+            console.log(modal, trigger);
+        },
+        complete: function() { console.log('Closed'); } // Callback for Modal close
+    });
+    //register
+    $('#registerModal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .3, // Opacity of modal background
+        inDuration: 750, // Transition in duration
+        outDuration: 700, // Transition out duration
+        startingTop: '100%', // Starting top style attribute
+        endingTop: '20%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+
+            console.log(modal, trigger);
+        },
+        complete: function() { console.log('Closed'); } // Callback for Modal close
+    });
+
+
+    $('#postModal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .3, // Opacity of modal background
+        inDuration: 700, // Transition in duration
+        outDuration: 700, // Transition out duration
+        startingTop: '0%', // Starting top style attribute
+        endingTop: '20%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+
+            console.log(modal, trigger);
+        },
+        complete: function() { console.log('Closed'); } // Callback for Modal close
+    });
+
+})
---
+++ app/shared/factories/redditFactory.js
@@ -0,0 +1,30 @@
+app.factory('redditFactory', ($q)=> {
+
+ return {
+    // addSong(title,artist,album,length) {
+    //   // console.log("addsong",title,artist, album, length)
+    //   return authFactory.getUser().then(user => {
+    //     console.log("addsong",title,artist, album, length)
+    //         // $scope.title, $scope.artist, $scope.album, $scope.length
+    //     return $http.post(`https://music-history-64fd9.firebaseio.com/Songs/.json`,
+    //     {
+    //       uid:user.uid,
+    //       Title: title,
+    //       Artist: artist,
+    //       Album: album,
+    //       Length: length
+    //     })
+    //   })
+    // },
+    // getSong() {
+    //   return authFactory.getUser().then(user => {
+    //     return $http.get(`https://music-history-64fd9.firebaseio.com/Songs/.json?orderBy="uid"&equalTo="${user.uid}"`)
+    //   })
+    }
+    // getPlaylist() {},
+    // addSong() {},
+    // addPlaylist(){},
+    // patchSongToPlaylist(){},
+    // deleteSong() {}
+
+  }
