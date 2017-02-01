d79e4b68961ecdab9eccab164c01ae8731c29a3d
joellegg <joellegg@bellsouth.net>
2017-02-01 13:32:40 -0600

    added ng-repeat to posts

--- app/shared/factories/redditFactory.js
+++ app/shared/factories/redditFactory.js
@@ -1,24 +1,24 @@
 app.factory('redditFactory', ($q, authFactory, $http) => {
 
-    return {
-        newPost(url, title) {
+  return {
+    newPost(url, title) {
 
-            return authFactory.getUser().then(user => {
-                console.log("addingpost")
-                    // $scope.title, $scope.artist, $scope.album, $scope.length
-                return $http.post(`https://reddit-steve.firebaseio.com/posts.json`, {
-                    uid: user.uid,
-                    Title: title,
-                    Link: url,
-                    // Name: first
-                    // Upvotes:,
-                    // Downvotes:,
-                    // Image:
-                })
-            })
-        },
-        getPosts() {
-            return $http.get(`https://reddit-steve.firebaseio.com/posts.json`)
-          }
+      return authFactory.getUser().then(user => {
+        console.log("addingpost")
+          // $scope.title, $scope.artist, $scope.album, $scope.length
+        return $http.post(`https://reddit-steve.firebaseio.com/posts.json`, {
+          uid: user.uid,
+          Title: title,
+          Link: url,
+          // Name: first
+          // Upvotes:,
+          // Downvotes:,
+          // Image:
+        })
+      })
+    },
+    getPosts() {
+      return $http.get(`https://reddit-steve.firebaseio.com/posts.json`)
     }
+  }
 })
